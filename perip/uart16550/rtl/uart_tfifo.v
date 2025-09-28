51b87a30401daa838103bac8e447d09c75ccdb82
kuikuikuizzZ <kimmmywu@gmail.com>
2025-09-28 13:09:45 +0800

    feat(soc): adapt to nvboard no gui

--- perip/uart16550/rtl/uart_tfifo.v
+++ perip/uart16550/rtl/uart_tfifo.v
@@ -211,7 +211,9 @@ begin
             begin
                 top   <= #1 top_plus_1;
                 count <= #1 count + 1'b1;
-                $write("%c", data_in);
+                `ifndef CONFIG_NVBOARD
+                    $write("%c", data_in);
+                `endif
             end
         2'b01 : if(count>0)
             begin
@@ -221,7 +223,9 @@ begin
         2'b11 : begin
                 bottom <= #1 bottom + 1'b1;
                 top    <= #1 top_plus_1;
-                $write("%c", data_in);
+                `ifndef CONFIG_NVBOARD
+                    $write("%c", data_in);
+                `endif
                 end
         default: ;
         endcase
